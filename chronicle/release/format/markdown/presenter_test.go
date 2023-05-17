20759bd04ceb573abab33b5d76d1ec76887e26a9
Cyber OS Lab <99838653+cyberoslab@users.noreply.github.com>
2023-05-17 16:37:27 -0700

    Update presenter_test.go
    Signed-off-by: Cyber OS Lab. <99838653+cyberoslab@users.noreply.github.com>

--- chronicle/release/format/markdown/presenter_test.go
+++ chronicle/release/format/markdown/presenter_test.go
@@ -52,8 +52,8 @@ func TestMarkdownPresenter_Present(t *testing.T) {
 						Version: "v0.19.1",
 						Date:    time.Date(2021, time.September, 16, 19, 34, 0, 0, time.UTC),
 					},
-					VCSReferenceURL: "https://github.com/nextlinux/syft/tree/v0.19.1",
-					VCSChangesURL:   "https://github.com/nextlinux/syft/compare/v0.19.0...v0.19.1",
+					VCSReferenceURL: "https://github.com/nextlinux/gosbom/tree/v0.19.1",
+					VCSChangesURL:   "https://github.com/nextlinux/gosbom/compare/v0.19.0...v0.19.1",
 					Changes: []change.Change{
 						{
 							ChangeTypes: []change.Type{change.NewType("bug", change.SemVerPatch)},
@@ -61,7 +61,7 @@ func TestMarkdownPresenter_Present(t *testing.T) {
 							References: []change.Reference{
 								{
 									Text: "456",
-									URL:  "https://github.com/nextlinux/syft/pull/456",
+									URL:  "https://github.com/nextlinux/gosbom/pull/456",
 								},
 							},
 						},
